a9204a3d0d2eeebaac8fd64dc5268f3b527bf37a
leiliang <liang.lei@mtime.com>
2017-02-27 16:58:11 +0800

    goodslist commit

--- wine/goodslist.js
+++ wine/goodslist.js
@@ -1,15 +1,28 @@
-//var app = angular.module("wine", []);
-//app.controller("com-header", function($scope) {
-//	document.getElementById("comHeader").innerHTML = GetComHeader(); 
-//});
-//
-//app.controller("com-footer", function($scope) {
-//	document.getElementById("comFooter").innerHTML = GetComFooter(); 
-//});
-
 
 var app = angular.module("goods", []);
 
 app.controller("com-header", function($scope) {
-	document.getElementById("comHeader").innerHTML = GetComHeader();
+	document.getElementById("comHeader").innerHTML = GetComHeader(); 
+});
+
+app.controller("com-footer", function($scope) {
+	document.getElementById("comFooter").innerHTML = GetComFooter(); 
+});
+
+app.controller("goods", function($scope, $http) {
+	
+	var swiper = new Swiper('.swiper-container', {
+		pagination: '.swiper-pagination',
+		paginationClickable: true,
+		autoplay: 2000,
+		loop: true
+	});
+	
+	$scope.asd = function() {
+		layer.open({
+  			content: '通过style设置你想要的样式'
+  			,style: 'background-color:#09C1FF; color:#fff; border:none;' //自定风格
+  			,time: 3
+		});
+	}
 });
