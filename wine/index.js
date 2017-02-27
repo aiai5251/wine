633b3173fe9e2cfb9465e703bcc1f891177e5850
shiyaorong <shiyaorong@didichuxing.com>
2017-02-27 11:56:40 +0800

    h5基本框架

---
+++ wine/index.js
@@ -0,0 +1,18 @@
+var app = angular.module("wine", []);
+app.controller("index", function($scope, $http) {
+	
+	var swiper = new Swiper('.swiper-container', {
+		pagination: '.swiper-pagination',
+		paginationClickable: true,
+		autoplay: 2000,
+		loop: true
+	});
+	
+	$scope.asd = function() {
+		layer.open({
+  			content: '通过style设置你想要的样式'
+  			,style: 'background-color:#09C1FF; color:#fff; border:none;' //自定风格
+  			,time: 3
+		});
+	}
+});
