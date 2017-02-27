42ae686c7b5549e1c7170d15a522732f669ef6f6
leiliang <liang.lei@mtime.com>
2017-02-27 16:29:58 +0800

    file path fix

---
+++ wine/goodslist.js
@@ -0,0 +1,15 @@
+//var app = angular.module("wine", []);
+//app.controller("com-header", function($scope) {
+//	document.getElementById("comHeader").innerHTML = GetComHeader(); 
+//});
+//
+//app.controller("com-footer", function($scope) {
+//	document.getElementById("comFooter").innerHTML = GetComFooter(); 
+//});
+
+
+var app = angular.module("goods", []);
+
+app.controller("com-header", function($scope) {
+	document.getElementById("comHeader").innerHTML = GetComHeader();
+});
