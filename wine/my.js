b31cccfe8576553cb4889563796a0f0b58b03f85
aiai5251 <934166463@qq.com>
2017-02-28 10:59:29 +0800

    个人中心

--- wine/my.js
+++ wine/my.js
@@ -4,11 +4,17 @@ app.controller("com-footer", function($scope) {
 });
 
 app.controller("my", function($scope, $http) {
+	$scope.orderStatusArray = [
+		{"title": "待付款", "icon": "waitpay.png"},
+		{"title": "待发货", "icon": "waitsend.png"},
+		{"title": "待收货", "icon": "waitpay.png"},
+		{"title": "已完成", "icon": "waitsend.png"}
+	];
 	
 	$scope.dataArray = [
 		{"title": "我的推广团队", "icon": "tuiguang.png", "hasRight": true},
 		{"title": "我的订单", "icon": "orderlist.png", "hasRight": false},
-		{"title": "客服", "icon": "tel.png", "hasRight": false},
+		{"title": "客服", "icon": "tel.png", "hasRight": false}
 	];
 	
 	
