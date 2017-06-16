c1e10e484aa0cee51c55383f0a05e0ec4cbf56c5
aiai5251 <934166463@qq.com>
2017-06-16 12:55:06 +0800

    删除无用文件

--- wine/com/com-headfooter.js
+++
@@ -1,7 +0,0 @@
-function GetComHeader() {
-	return "<div style=\"background-color: #1b1b1f; color: white; position: fixed; height: 38px; width: 100%; z-index: 999;\"><a href=\"\" style=\"color: white;\"><img width=\"24\" height=\"24\" src=\"img/logo.png\" style=\"margin: 7px 5px 0px 5px;\"><span style=\"font-size: 13px; color: white; vertical-align: top; line-height: 38px;\">东方酒业</span></a><div style=\"clear: both;\"></div></div>";
-}
-
-function GetComFooter() {
-	return "<div style=\"position: fixed; left:0px; right:0px; bottom: 0px; background-color: white;\"><a><img src=\"img/tab/typeSel1.jpg\" style=\"width: 24%; height: 24vm;\" /></a><a href=\"cart.html\"><img src=\"img/tab/type2.jpg\" style=\"width: 24%; height: 24vm\" /></a><a href=\"my.html\"><img src=\"img/tab/type3.jpg\" style=\"width: 24%; height: 24vm\" /></a><a href=\"my_qrcode.html\"><img src=\"img/tab/type4.jpg\" style=\"width: 24%; height: 24vm\" /></a></div>"
-}
