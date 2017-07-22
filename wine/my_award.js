b06e46cfb407ee458d6d8b8b85d8a8d66d1641a4
leiliang <liang.lei@mtime.com>
2017-07-22 12:56:19 +0800

    奖励提交

--- wine/my_award.js
+++ wine/my_award.js
@@ -1,4 +1,8 @@
 var app = angular.module("wine", []);
 app.controller("my_award", function($scope, $http) {
-	 
+	$scope.uid = getUid();
+	 $http.get(getHeadUrl() + "mineController/MineRewardList.do?uid=" + $scope.uid).success(function(response) {
+	 	console.log(response);
+	 	$scope.listReward = response.listReward;
+	 });
 });
