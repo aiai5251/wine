a62abf5cf5e8f3cce0d98c4ebaa048e10574ecb7
leiliang <liang.lei@mtime.com>
2017-07-22 12:34:47 +0800

    优惠券列表请求

--- wine/coupon.js
+++ wine/coupon.js
@@ -1,4 +1,9 @@
 var app = angular.module("wine", []);
 app.controller("coupon", function($scope, $http) {
-	 
+	 $scope.pid = GetQueryInt("pid"); 
+	 $scope.uid = getUid();
+	 $http.get(getHeadUrl() + "mineController/MineCouPon.do?uid=" + $scope.uid).success(function(response) {
+		console.log(response);
+		$scope.couponList = response.listCoupon;
+	});
 });
